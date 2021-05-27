1a87acb3b88760ddb8fe3829c8a04eb8b14f43f7
Gildas GH <gildas.gh@protonmail.ch>
2021-05-27 11:04:09 +0200

    Created YNH app for OnlyOffice Unlocked

---
+++ README.md
@@ -0,0 +1,14 @@
+OnlyOffice Docker for Yunohost
+==========
+
+[![Install OnlyOffice Docker with YunoHost](https://install-app.yunohost.org/install-with-yunohost.png)](https://install-app.yunohost.org/?app=onlyoffice docker)
+
+OnlyOffice Unlocked with Docker
+
+**[See more]()**
+
+This app is inside a [Docker image](alehoho/oo-ce-docker-license).
+
+-----------------
+
+*It was generated with [DockerApp Yunohost](https://github.com/aymhce/dockerappmodel_ynh/)*
---
+++ scripts/docker/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# example :
+#
+#[ "$architecture" == "amd64" ] && image=portainer/portainer:1.14.0
+#[ "$architecture" == "i386" ]  && image=portainer/portainer:linux-386-1.14.0
+#[ "$architecture" == "armhf" ] && image=portainer/portainer:linux-arm-1.14.0
+#[ -z $image ] && ynh_die "Sorry, your $architecture architecture is not supported ..."
+#
+#options="-p $port:9000 -v $data_path/data:/data"
+#
+#docker run -d --name=$app --restart always $options $image 1>&2
+#echo $?
+
+docker run \
+    --name=$app \
+    --detach \
+    --publish=$port:80 \
+    alehoho/oo-ce-docker-license 1>&2
+
+echo $?
